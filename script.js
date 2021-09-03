0bfc09612161823c007a373e11d108263eb93138
Utkarsh Dixit <88888678+lucky-dixit@users.noreply.github.com>
2021-09-03 11:28:12 +0530

    Script file
    A simple JS for scroll-up buttons and few more.

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ $(document).ready(function () {
         $('.menu-btn i').toggleClass("active");
     });
 
+    
     // typing text animation script
     var typed = new Typed(".typing", {
         strings: ["Web Developer...", "Designer", "Freelancer"],
