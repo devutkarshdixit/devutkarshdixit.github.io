2163f9cc6c55953709f67a4437015cf71e663a98
Utkarsh Dixit <88888678+utkarsh-dixit-git@users.noreply.github.com>
2021-11-17 16:28:25 +0530

    Script file
    A simple JS for scroll-up buttons and few more.

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function () {
     
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer...", "Designer", "Freelancer"],
+        strings: ["Web Developer...", "Designer"],
         typeSpeed: 100,
         backSpeed: 100,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer...", "Designer", "Freelancer"],
+        strings: ["Web Developer...", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
