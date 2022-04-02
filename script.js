ae21c26a3b345f887ff67afef7503bac98e487de
Utkarsh Dixit <utkarshdixit2021@gmail.com>
2022-04-02 23:00:37 +0530

    Script

--- script.js
+++ script.js
@@ -33,20 +33,6 @@ $(document).ready(function () {
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Web Developer...", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 100,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["Web Developer...", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
 
     // owl carousel script
     $('.carousel').owlCarousel({
