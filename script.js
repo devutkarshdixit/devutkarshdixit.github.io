844faa3a717b181ef5ec29308bd8c0feb1734c61
Utkarsh Dixit <utkarshdixit2021@gmail.com>
2022-11-25 20:21:33 +0530

    Update
    Exp and Style update

--- script.js
+++ script.js
@@ -34,14 +34,14 @@ $(document).ready(function () {
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer...", "Designer", "Freelancer"],
+        strings: ["Web Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 100,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer...", "Designer", "Freelancer"],
+        strings: ["Web Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
